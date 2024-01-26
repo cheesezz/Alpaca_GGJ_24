20e7f10d00743bca1b97b245fc736ec4399771b5
Alexi / Zach <zwong00@gmail.com>
2024-01-27 01:04:48 +0800

    Added Basic entity classes to identify, plus additional module for input

--- Assets/Scripts/Entity System/EntityBase.cs
+++ Assets/Scripts/Entity System/EntityBase.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class EntityBase : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
         
     }
