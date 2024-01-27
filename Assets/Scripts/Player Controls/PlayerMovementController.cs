7920ae3148f2f81861fe90fb5026972aef7a0502
Alexi (Zach) <zwong00@gmail.com>
2024-01-27 13:29:48 +0800

    Re-added facing direction in PMC

--- Assets/Scripts/Player Controls/PlayerMovementController.cs
+++ Assets/Scripts/Player Controls/PlayerMovementController.cs
@@ -9,6 +9,8 @@ public class PlayerMovementController : MonoBehaviour
 
     [SerializeField] private PlayerPhysics privatePlayerPhysics;
 
+    public Vector2 facingDirection = Vector2.zero;
+
     // Start is called before the first frame update
     void Start()
     {
