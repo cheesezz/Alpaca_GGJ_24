87b03037d90cee804bd5bdcda5d268b83cbde535
HavocGrapeSoda <havocisuponyou@gmail.com>
2024-01-28 11:28:35 +0800

    Fixed slapping bug and implemented slamming

--- Assets/Scripts/Physics/PlayerPhysics.cs
+++ Assets/Scripts/Physics/PlayerPhysics.cs
@@ -10,6 +10,8 @@ public class PlayerPhysics : MonoBehaviour
     [SerializeField] float slapForce = 500f;
     [Range(0.01f, 1f)]
     [SerializeField] float slapVertical = 0.1f;
+    private bool slapNow = false;
+    private bool slamNow = false;
     public bool canJump = false;
 
     // Start is called before the first frame update
@@ -53,9 +55,25 @@ public class PlayerPhysics : MonoBehaviour
     public void OnSlap()
     {
         Vector2 facing = movementController.facingDirection;
+        if (slapObjectContainer != null) { return; }
+
+        Debug.Log("Slapping!");
         slapObjectContainer = Instantiate(slapObject,
             new Vector2(gameObject.transform.position.x, gameObject.transform.position.y) + facing,
             slapObject.transform.rotation, transform);
+
+        slapNow = true;
+    }
+
+    public void OnSlam()
+    {
+        Vector2 facing = movementController.facingDirection;
+        if (slapObjectContainer != null) { return; }
+
+        Debug.Log("Slamming!");
+        slapObjectContainer = Instantiate(slapObject, new Vector2(gameObject.transform.position.x, gameObject.transform.position.y) + facing, slapObject.transform.rotation, transform);
+
+        slamNow = true;
     }
 
     /// <summary>
@@ -63,30 +81,56 @@ public class PlayerPhysics : MonoBehaviour
     /// object (2D physics only).
     /// </summary>
     /// <param name="other">The other Collider2D involved in this collision.</param>
-    void OnTriggerEnter2D (Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.layer == 6)
         {
-            Slap(other);
+            if (slapNow && !slamNow)
+            {
+                Slap(other);
+                slapNow = false;
+            }
+            else if (!slapNow && slamNow)
+            {
+                Slam(other);
+                slamNow = false;
+            }
         }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        
+
     }
 
+    /// <summary>
+    /// Performs the physics for the slapping
+    /// </summary>
+    /// <param name="collision"></param>
     void Slap(Collider2D collision)
     {
         if (collision == null || collision.gameObject == this.gameObject)
             return;
 
-        Vector2 dir = new Vector2(gameObject.transform.position.x - collision.transform.position.x,
-            gameObject.transform.position.y - collision.transform.position.y + slapVertical);
+        Vector2 dir = new Vector2(collision.transform.position.x - gameObject.transform.position.x,
+            collision.transform.position.y - gameObject.transform.position.y + slapVertical);
 
         dir = new Vector2(slapForce * dir.x, slapForce * dir.y);
-        
-        gameObject.GetComponent<Rigidbody2D>().AddForce(dir);
+
+        Debug.Log("PlayerPhysics");
+        collision.attachedRigidbody.AddForce(dir);
         Debug.Log(dir);
+    }
+
+    void Slam(Collider2D collision)
+    {
+        if (collision == null || collision.gameObject == this.gameObject) return;
+
+        //slam downwards only
+        collision.attachedRigidbody.velocity = Vector2.zero;    //dead stop the being-hit
+        Vector2 dir = new Vector2(Vector2.down.x * slapForce, Vector2.down.y * slapForce);
+
+        //Debug.Log("slam force: " + dir);
+        collision.attachedRigidbody.AddForce(dir);
     }
 }
